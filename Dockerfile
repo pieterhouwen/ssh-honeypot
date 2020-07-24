2a15e8585ba0d04e1c5f4315e26b810178d4ca35
Pieter Houwen <pieter@pieterhouwen.info>
2020-07-25 01:27:52 +0200

    Other changes

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,4 @@
 FROM ubuntu:18.04
 COPY . /tmp/
-RUN apt update
-RUN apt install wget -y
-RUN alias wget='wget --no-check-certificate'
-RUN apt purge openssl -y
-RUN apt install build-essential gcc glibc-source make -y
 RUN bash /tmp/prep.sh
+CMD tail -f /dev/null
--- prep.sh
+++ prep.sh
@@ -1,19 +1,19 @@
+wget='wget --no-check-certificate'
 OPENSSH=/opt/openssh2
 mkdir -p /opt/openssh2/dist/
 cd ${OPENSSH}
-wget http://zlib.net/zlib-1.2.11.tar.gz
+$wget http://zlib.net/zlib-1.2.11.tar.gz
 tar xvfz zlib-1.2.11.tar.gz
 cd zlib-1.2.11
 ./configure --prefix=${OPENSSH}/dist/ && make && make install
 cd ${OPENSSH}
-alias wget='wget --no-check-certificate'
-wget http://www.openssl.org/source/openssl-1.0.1e.tar.gz
+$wget http://www.openssl.org/source/openssl-1.0.1e.tar.gz
 tar xvfz openssl-1.0.1e.tar.gz
 cd openssl-1.0.1e
 cp -f /tmp/Makefile.org .
 ./config --prefix=${OPENSSH}/dist/ && make && make install
 cd ${OPENSSH}
-wget https://ftp.eu.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.2p1.tar.gz
+$wget https://ftp.eu.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-6.2p1.tar.gz
 tar xvfz openssh-6.2p1.tar.gz
 cd openssh-6.2p1
 sed -e 's/struct passwd \* pw = authctxt->pw;/logit("Honey: Username: %s Password: %s", authctxt->user, password);\nstruct passwd \* pw = authctxt->pw;/' -i auth-passwd.c
